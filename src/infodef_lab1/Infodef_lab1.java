7b6f3c8e24503dca25ce2e7c5a3c34dd67e64600
PavelVorobyev97 <pavelvorobyev97@yandex.ru>
2018-09-10 22:43:31 +0300

    Seond with bigrams

--- src/infodef_lab1/Infodef_lab1.java
+++ src/infodef_lab1/Infodef_lab1.java
@@ -16,6 +16,7 @@ public class Infodef_lab1 {
 
         //хэшмап для хранения символов и частоты их появления
         HashMap<Character, Double> freq = new HashMap<>();
+
         for (int i = 0; i < Input.lang.size(); i++) {
             freq.put(Input.lang.get(i), 0.0);
         }
@@ -23,7 +24,7 @@ public class Infodef_lab1 {
         //считаем частоту символов 
         for (int i = 0; i < Input.input.length(); i++) {
             if (Character.UnicodeBlock.of(Input.input.charAt(i)) == Character.UnicodeBlock.CYRILLIC) {
-                freq.put(Input.input.toUpperCase().charAt(i), freq.get(Input.input.toUpperCase().charAt(i)) + 1); //прибавление единицы к массиву значений 
+                freq.put(Input.input.charAt(i), freq.get(Input.input.charAt(i)) + 1); //прибавление единицы к массиву значений 
             }
         }
         for (Map.Entry<Character, Double> entry : freq.entrySet()) {
@@ -34,12 +35,14 @@ public class Infodef_lab1 {
         for (int i = 0; i < Input.input.length(); i++) { //цикл по входной строке
             if (Character.UnicodeBlock.of(Input.input.charAt(i)) == Character.UnicodeBlock.CYRILLIC) {
                 for (int j = 0; j < Input.lang.size(); j++) { //цикл по алфавиту
-                    if (Input.input.toUpperCase().charAt(i) == Input.lang.get(j)) {
+                    if (Input.input.charAt(i) == Input.lang.get(j)) {
                         if (Input.offset + j >= Input.lang.size()) {
                             output.append(Input.lang.get(
                                     (j + Input.offset) - Input.lang.size()));
+                            break;//???
                         } else {
                             output.append(Input.lang.get(j + Input.offset));
+                            break;//???
                         }
                     }
                 }
@@ -52,9 +55,9 @@ public class Infodef_lab1 {
         //шифруем частотой
         for (int i = 0; i < Input.input.length(); i++) {
             if (Character.UnicodeBlock.of(Input.input.charAt(i)) == Character.UnicodeBlock.CYRILLIC) {
-                output_freq.append(freq.get(Input.input.toUpperCase().charAt(i)) + " ");
+                output_freq.append(freq.get(Input.input.charAt(i)) + " ");
 
-                outputFreqArr.add(freq.get(Input.input.toUpperCase().charAt(i)) + " ");
+                outputFreqArr.add(freq.get(Input.input.charAt(i)) + " ");
             } else {
                 output_freq.append(Input.input.charAt(i));
 
@@ -81,14 +84,90 @@ public class Infodef_lab1 {
             }
         }
 
+        /*
+        //Биграммы
+        HashMap<String, Double> freqBig = new HashMap<>();
+        ArrayList<String> outputFreqBigArr = new ArrayList<>();
+        StringBuilder outputFreqBigStr = new StringBuilder();
+        
+        //заполняем биграмами 
+        for (int i = 0; i < Input.input.length() - 1; i++) {
+            if (Character.UnicodeBlock.of(Input.input.charAt(i)) == Character.UnicodeBlock.CYRILLIC
+                    && Character.UnicodeBlock.of(Input.input.charAt(i + 1)) == Character.UnicodeBlock.CYRILLIC) {
+                freqBig.put(String.valueOf(Input.input.charAt(i)) + String.valueOf(Input.input.charAt(i + 1)), 0.0);
+            }
+        }
+
+        //считаем количество биграмм 
+        for (Map.Entry<String, Double> entry : freqBig.entrySet()) {
+            for (int i = 0; i < Input.input.length() - 1; i++) {
+                if ((String.valueOf(Input.input.charAt(i)) + String.valueOf(Input.input.charAt(i + 1))).equals(entry.getKey())) {
+                    freqBig.put(entry.getKey(), entry.getValue() + 1);
+                }
+            }
+        }
+        for (Map.Entry<String, Double> entry : freqBig.entrySet()) { //делим на общее количество символов строки 
+            freqBig.put(entry.getKey(), entry.getValue() / Input.input.length());
+        }
+
+        //шифруем биграмаами 
+        for (int i = 0; i < Input.input.length() - 1; i ++ ) {
+            if(Character.UnicodeBlock.of(Input.input.charAt(i)) == Character.UnicodeBlock.CYRILLIC
+                    && Character.UnicodeBlock.of(Input.input.charAt(i + 1)) == Character.UnicodeBlock.CYRILLIC){
+                
+                String tmp = String.valueOf(Input.input.charAt(i)) + String.valueOf(Input.input.charAt(i + 1));
+                
+                outputFreqBigArr.add(freqBig.get(tmp).toString()); //в массив
+                outputFreqBigStr.append(freqBig.get(tmp).toString()); //в строку
+                
+                i++;
+                
+            }
+            if(Character.UnicodeBlock.of(Input.input.charAt(i + 1)) != Character.UnicodeBlock.CYRILLIC){
+                outputFreqBigArr.add(String.valueOf(Input.input.charAt(i + 1)));
+                outputFreqBigStr.append(String.valueOf(Input.input.charAt(i + 1)) + " ");
+            }
+                
+        }
+        
+        //расшифровываем биграммы
+        
+        StringBuilder decodedBigFreq = new StringBuilder();
+        for (String str : outputFreqBigArr) {
+            for (Map.Entry<String, Double> entry : freqBig.entrySet()) {
+                if (str.length() > 1) {
+                    if (Double.parseDouble(str) == entry.getValue()) {
+                        decodedBigFreq.append(entry.getKey());
+                        break;
+                    }
+                } else {
+                    decodedBigFreq.append(str);
+                    break;
+                }
+            }
+        }
+        */
+        
+        /*
+        System.out.println("========");
+        System.out.println(freqBig);
+        System.out.println(outputFreqBigArr);
+        System.out.println(outputFreqBigStr+ " ");
+        */
+        
+
         //ДЛЯ ДЕБАГА
         //System.out.println(freq); //МАП с частотой 
         //System.out.println("Частота выход: " + output_freq);
         //System.out.println("Частота выход(лист): " + outputFreqArr);
+        
         System.out.println("Вход: " + Input.input);
         System.out.println("Сдвиг выход: " + output);
         System.out.println("Частота выход: " + output_freq);
         System.out.println("Расшифровка частотного: " + decodedFreq);
+        //System.out.println("Расшифровка частотного биграммы: " + decodedBigFreq);
+        //System.out.println(freqBig);
+        
         /*
         //расшифровываем сдвиг А ОНО НАДО ВООБЩЕ? ИБО НАФИГА?
         StringBuilder output2 = output;
@@ -96,7 +175,6 @@ public class Infodef_lab1 {
             for (int j = 0; j < Input.lang.size(); j++) {
                 if(output2.charAt(i) == Input.lang.get(j)){
                     output2.replace(j, j, Input.lang.get(j-2).toString());
-                    System.out.println("+");
                 }
             }
         }
